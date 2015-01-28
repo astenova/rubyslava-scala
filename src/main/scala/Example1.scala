9ff31eacf05959a1f873c7757045886781f19780
Andrea Stenova <andrea.stenova@gmail.com>
2015-01-28 20:17:06 +0100

    first examples added

--- src/main/scala/Example1.scala
+++ src/main/scala/Example1.scala
@@ -11,13 +11,70 @@ object Example1 {
       case Nil => "the empty list"
       case _ => "something else"
     }
-
+    println
+    println("CONSTANTS")
+    println
     println(describe(5))
     println(describe(true))
     println(describe("hello"))
     println(describe(Nil))
     println(describe(false))
+    println
 
+    def isZero(expr: Any) = expr match {
+      case 0 => "zero"
+      case somethingElse => "not zero: "+ somethingElse
+    }
+
+    println("VARIABLES")
+    println
+    println(isZero(0))
+    println(isZero(1))
+    println(isZero(0.0))            // Double conversion
+    println(isZero(BigDecimal(0)))  // BigDecimal conversion
+    println(isZero(false))
+    println
+
+    def listFun(list: Any) = list match {
+      case List(0, _, _) =>
+        list + " starts with 0 and has 3 elements"
+
+      case List(_, a: String) =>
+        list + " ends with string: " + a + " and has 2 elements"
+
+      case List(0, _*) =>
+        list + " starts with 0"
+
+      case _ : List[Any] =>
+        list + " is random list"
+
+      case x =>
+        x + " is not a list"
+    }
+
+    println("LISTS")
+    println
+    println(listFun(List(1, 2, 3)))
+    println(listFun(List(0, 2, 3)))
+    println(listFun(List(1, "hello")))
+    println(listFun(List(0, 2)))
+    println(listFun(1))
+    println
+
+    def recursion(list: List[Int]) : Unit = list match {      // need result type
+      case head :: tail => {
+        println(head)
+        recursion(tail)
+      }
+      case Nil => println("end")
+    }
+
+    recursion(List(1, 2, 3))
+    println
+    recursion(List())
+    println
+    recursion(List(1, 2, 3, 4, 5))
+    println
 
   }
 }
--- src/main/scala/Example2.scala
+++ src/main/scala/Example2.scala
@@ -1,5 +1,43 @@
 object Example2 {
   def main(args: Array[String]): Unit = {
-    println("hello")
+    sealed trait User
+
+    case class Admin(
+                      name: String,
+                      surname: String) extends User
+
+    case class Employee(
+                         name: String,
+                         surname: String, age: Int) extends User
+
+    case class Customer(
+                         name: String,
+                         surname: String,
+                         isImportant: Boolean) extends User
+
+    def getUserInfo(user: User) = user match {
+      case Admin(name, surname) =>
+        "admin: " + name + " " + surname
+
+      case Employee(name, surname, age) =>
+        "employee: " + name + " " + surname + " with age " + age
+
+      //case Customer(name, surname, importance) =>
+      //  "customer: " + name + " " + surname
+
+      case Customer(name, surname, true) =>
+        "important customer: " + name + " " + surname
+
+      case Customer(name, surname, false) =>
+        "regular customer: " + name + " " + surname
+    }
+
+    println("CLASSES")
+    println
+    println(getUserInfo(Admin("Peter","Smith")))
+    println(getUserInfo(Employee("Martin","Brown", 35)))
+    println(getUserInfo(Customer("John", "Meier", false)))
+    println(getUserInfo(Customer("Ann", "Jones", true)))
+    println
   }
 }
